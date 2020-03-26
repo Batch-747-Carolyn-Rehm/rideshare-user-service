5880fe7c979cceead482511c5142919eb3c45e3f
Tianyu Li <alandron06281990@gmail.com>
2020-03-26 14:11:58 -0700

    add comment

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -278,7 +278,7 @@ public class UserController {
 	@ApiOperation(value="Updates user by id", tags= {"User"})
 	@PutMapping
 	public ResponseEntity<Map> updateUser(@Valid @RequestBody User user, BindingResult result) {
-		//System.out.println(user);
+		//validating address using custom validator
 		uv.validate(user, result);
 		System.out.println("validation result is " + result);
 		
