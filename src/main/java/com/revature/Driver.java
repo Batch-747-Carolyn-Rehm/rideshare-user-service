495f25a9df9238f34f735f87963a4557e1f656b8
Tianyu Li <alandron06281990@gmail.com>
2020-03-26 14:09:10 -0700

    implement address validation utilizing Google GeoLocation API

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -35,11 +35,7 @@ public class Driver {
 	public static void main(String[] args) {
 		SpringApplication.run(Driver.class, args); 
 		
-		//test making request to GeoLocationAPI and parse response
-		RestTemplate restTemplate = new RestTemplate();
-		String url = "https://maps.googleapis.com/maps/api/geocode/json?address=1001+s+center+st,+Arlington+TX+76010&key=AIzaSyDdU7KMs5IfYmqJXmYZc8MzL_8fN3z5Hec";
-		GeoLocationResponse gr = restTemplate.getForObject(url, GeoLocationResponse.class);
-		System.out.println(gr.getResults().get(0).getFormatted_address());
+		
 	}
 	
 	/**
