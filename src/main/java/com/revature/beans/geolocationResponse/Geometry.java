38c2c95a34f4f4710375acaa8bcec2deb9dd8119
Tianyu Li <alandron06281990@gmail.com>
2020-03-26 12:44:16 -0700

    successfully send http request with restTemplate and successfully parse response json

---
+++ src/main/java/com/revature/beans/geolocationResponse/Geometry.java
@@ -0,0 +1,43 @@
+package com.revature.beans.geolocationResponse;
+
+import java.io.Serializable;
+
+public class Geometry implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Location location;
+	private String location_type;
+	private Viewport viewport;
+	
+	public Geometry() {
+		super();
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+
+	public String getLocation_type() {
+		return location_type;
+	}
+
+	public void setLocation_type(String location_type) {
+		this.location_type = location_type;
+	}
+
+	public Viewport getViewport() {
+		return viewport;
+	}
+
+	public void setViewport(Viewport viewport) {
+		this.viewport = viewport;
+	}
+
+}
---
+++ src/main/java/com/revature/beans/geolocationResponse/Location.java
@@ -0,0 +1,28 @@
+package com.revature.beans.geolocationResponse;
+
+import java.io.Serializable;
+
+public class Location implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String lat;
+	private String lng;
+	public Location() {
+		super();
+	}
+	public String getLat() {
+		return lat;
+	}
+	public void setLat(String lat) {
+		this.lat = lat;
+	}
+	public String getLng() {
+		return lng;
+	}
+	public void setLng(String lng) {
+		this.lng = lng;
+	}
+	
+}
