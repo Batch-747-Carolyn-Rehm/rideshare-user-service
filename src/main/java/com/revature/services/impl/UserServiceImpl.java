de8d464dd9f2806c984dcbf067bb9c90ac4940a3
Vytautas Klimavicius <vyklimavicius@gmail.com>
2020-03-27 09:14:05 -0500

    Initial logging services/exception

--- src/main/java/com/revature/services/impl/UserServiceImpl.java
+++ src/main/java/com/revature/services/impl/UserServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.revature.beans.User;
 import com.revature.repositories.UserRepository;
 import com.revature.services.UserService;
@@ -36,6 +35,7 @@ public class UserServiceImpl implements UserService {
 	
 	@Override
 	public List<User> getUsers() {
+		
 		return ur.findAll();
 	}
 
