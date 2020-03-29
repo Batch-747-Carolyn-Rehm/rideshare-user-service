7a5d3880b0ed43094814282de14e3e0133a705dc
Matthias Quintero <matthiasquintero@gmail.com>
2020-03-28 21:15:02 -0600

    Filter returns response entity

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -11,6 +11,8 @@ import java.util.Set;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -20,10 +22,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.maps.errors.ApiException;
 import com.revature.beans.Filter;
 import com.revature.beans.User;
@@ -61,6 +65,8 @@ public class UserController {
 	
 	@Autowired
 	private FilterService fs;
+	
+	private ObjectMapper mapper = new ObjectMapper();
 	
 	/**
 	 * HTTP GET method (/users)
@@ -119,8 +125,8 @@ public class UserController {
 	//Get Drivers by different filters
 	
 	@ApiOperation(value="Returns drivers by filter",tags= {"User"})
-	@GetMapping("/filter")
-	public Set<User> getFilteredDrivers(@RequestBody Filter filters)
+	@PostMapping("/filter")
+	public ResponseEntity<String> getFilteredDrivers(@RequestBody Filter filters)
 			throws ApiException, InterruptedException, IOException{
 		Set<User> totalDrivers = new HashSet<User>();
 		String[] fullAddress = filters.getLocation().split(", ");
@@ -149,7 +155,7 @@ public class UserController {
 				}
 			}
 		}
-		return totalDrivers;
+		return ResponseEntity.ok().body("{ \"Drivers\": " + mapper.writeValueAsString(totalDrivers)+"}");
 	}
 	
 	
