d660b5459e554f1bb90cc41e7c6d3863a307c145
Chris Charnecki <christopher.charnecki@gmail.com>
2020-03-31 16:23:38 -0500

    removed unused imports

--- src/main/java/com/revature/beans/Car.java
+++ src/main/java/com/revature/beans/Car.java
@@ -7,12 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
-import javax.validation.constraints.Size;
 
 import org.springframework.stereotype.Component;
 
