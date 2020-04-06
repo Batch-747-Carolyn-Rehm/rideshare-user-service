d6f0a307917c2e83b9b292c9c139485d4ad5a3b0
met2224 <metsang@utexas.edu>
2020-04-06 10:34:07 -0500

    base filter by available seats and distance less than 5 miles

--- src/main/java/com/revature/services/impl/UserServiceImpl.java
+++ src/main/java/com/revature/services/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -20,6 +21,7 @@ import com.revature.services.DistanceService;
 import com.revature.services.FilterService;
 import com.revature.services.UserService;
 import com.revature.utils.UserComparator;
+
 
 /**
  * UserServiceImpl handles any additional services that need to be made before calling the
@@ -159,6 +161,7 @@ public class UserServiceImpl implements UserService {
 		if (distanceCache.containsKey(currentUser.getUserId())) {
 			// Set totalDrivers to list from cache
 			totalDrivers = new HashSet<User>(distanceCache.get(currentUser.getUserId()));
+			
 		} else {
 			// Pull from DB, run through distanceMatrix, add to cache
 			List<User> allDrivers = getActiveDrivers();
@@ -182,6 +185,14 @@ public class UserServiceImpl implements UserService {
 			String fullAddress = currentUser.gethAddress() + ", " + currentUser.gethCity() + ", " + currentUser.gethState();
 			try {
 				totalDrivers = fs.filterByRecommendation(fullAddress, filters.getBatchId(), totalDrivers);
+
+				Iterator<User> i = totalDrivers.iterator();
+				while(i.hasNext() ) {
+					User u = i.next();
+					if(u.getDistance() > 8046.72 || u.getUserId() == currentUser.getUserId() || u.getCar().getSeatsAvailable() == 0) {
+						i.remove();
+					}
+				}
 			} catch (ApiException | InterruptedException | IOException e) {
 				e.printStackTrace();
 			}
